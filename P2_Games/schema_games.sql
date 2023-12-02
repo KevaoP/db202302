c21be926f7f309b32d6003a91a3bde4a9735199d
KevaoP Pompeu <kevin.santos9@fatec.sp.gov.br>
2023-12-02 13:33:50 +0000

    Prova_2

--- P2_Games/schema_games.sql
+++ P2_Games/schema_games.sql
@@ -1,25 +1,44 @@
 -- Criando o Banco de Dados
 CREATE DATABASE games;
 
--- "entrando" para realizar as alterações
+-- "entrando" Para realizar as alterações
 USE games;
 
+-- Criando tabela de categorias
 CREATE TABLE categorias(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
 );
 
+-- Criando tabela de jogos
 CREATE TABLE jogos(
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
-    competitivo BOOLEAN DEFAULT false
+    competitivo BOOLEAN NOT NULL,
+    id_categoria INT NOT NULL,
+    FOREIGN KEY (id_categoria) REFERENCES categorias (id)
 );
 
+-- Criando tabela de plataformas
 CREATE TABLE plataformas(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
+    nome VARCHAR(255) NOT NULL,
+    id_jogo INT NOT NULL,
+    FOREIGN KEY (id_jogo) REFERENCES jogos (id)
 );
 
-SHOW TABLES FROM games;
+-- Criando relação N para N (jogos e plataformas)
+CREATE TABLE jogos_possuem_plataformas(
+    id_jogo INT NOT NULL,
+    id_plataforma INT NOT NULL,
+    PRIMARY KEY (id_plataforma, id_jogo),
+    FOREIGN KEY (id_plataforma) REFERENCES plataformas (id),
+    FOREIGN KEY (id_jogo) REFERENCES jogos (id)
+);
 
+-- Mostrando as tabelas criadas
+SHOW TABLES FROM games;
 SHOW FIELDS FROM categorias;
+SHOW FIELDS FROM jogos;
+SHOW FIELDS FROM plataformas;
+SHOW FIELDS FROM jogos_possuem_plataformas;
